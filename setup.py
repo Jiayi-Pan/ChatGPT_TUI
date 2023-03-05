9a5b0bd6b8076b0de81766563bdcecfe93dd55bc
Jiayi Pan <i@jiayipan.me>
2023-03-04 22:21:36 -0500

    Move to setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chatgpt_tui",
+    version="0.5.2",
+    author="Jiayi Pan",
+    author_email="i@jiayipan.me",
+    description="A simple TUI interface for ChatGPT.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jiayi-Pan/ChatGPT_TUI",
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["chatbot", "tui", "chatgpt", "chatgpt_tui"],
+    packages=find_packages(),
+    install_requires=["textual", "httpx", "pyperclip"],
+    entry_points={
+        "console_scripts": [
+            "catt = chatgpt_tui.app:run"
+        ]
+    },
+    package_data={'': ['*.css']}
+)
